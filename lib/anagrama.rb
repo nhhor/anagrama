3a58020e8c50f81d162901fc59e50666f7c622ea
Noah Horwitz <nhhor@post.com>
2019-12-13 11:59:56 -0800

    refactor tests and class init to make better use of instance vars. now comparing obj.self to one anagram-meth-arg.

--- lib/anagrama.rb
+++ lib/anagrama.rb
@@ -2,25 +2,27 @@ class ANAGRAM
     
   # attr_reader(:)
   # attr_writer(:)
-  # attr_accessor(:)
+  attr_accessor(:primary_word)
 
-  # def initialize()
-  # end
+  def initialize(primary_word)
+    @primary_word = primary_word
+  end
 
-  def anagram(word1, word2)
-    sorted_letters1 = word1.downcase.split("").sort
+
+  def anagram(word2)
+    sorted_letters1 = primary_word.downcase.split("").sort
     sorted_letters2 = word2.downcase.split("").sort
     letters_compare = sorted_letters1 & sorted_letters2
 
     # puts `clear`
     puts ""
-    puts "LOG word1: '#{word1}' / LOG sorted_letters1: #{sorted_letters1}"
+    puts "LOG primary_word: '#{primary_word}' / LOG sorted_letters1: #{sorted_letters1}"
     puts "LOG word2: '#{word2}' / LOG sorted_letters2: #{sorted_letters2}"
 
     puts "LOG sorted_letters1 length: #{sorted_letters1.length} sorted_letters2 length: #{sorted_letters2.length}"
     puts "LOG letters_compare: #{letters_compare} letters_compare length: #{letters_compare.length}"
 
-    if (word1.match(/[aeiouy]/i) && word2.match(/[aeiouy]/i))
+    if (primary_word.match(/[aeiouy]/i) && word2.match(/[aeiouy]/i))
       if (sorted_letters1 == sorted_letters2)
         answer = "These words are anagrams."
       elsif letters_compare.length == 0
--- spec/anagrama_spec.rb
+++ spec/anagrama_spec.rb
@@ -4,28 +4,28 @@ require('anagrama.rb')
 describe('ANAGRAM#checker') do
 
   it("1) returns 'These words are anagrams.' when anagram words are entered.") do
-    test = ANAGRAM.new()
-    expect(test.anagram("not", "ton")).to(eq("These words are anagrams."))
+    test = ANAGRAM.new("not")
+    expect(test.anagram("ton")).to(eq("These words are anagrams."))
   end
 
   it("2) returns 'These words are anagrams.' when anagram words are entered regardless of letter-case.") do
-    test = ANAGRAM.new()
-    expect(test.anagram("Tea", "Eat")).to(eq("These words are anagrams."))
+    test = ANAGRAM.new("Tea")
+    expect(test.anagram("Eat")).to(eq("These words are anagrams."))
   end
 
   it("3) returns 'You need to input actual words!' when either word entered does not contain at least one vowel.") do
-    test = ANAGRAM.new()
-    expect(test.anagram("Ruby", "hbnR")).to(eq("You need to input actual words!"))
+    test = ANAGRAM.new("Ruby")
+    expect(test.anagram("hbnR")).to(eq("You need to input actual words!"))
   end
 
   it("4) returns 'These words have no letter matches and are antigrams.' when either word entered does not contain at least one similar letter.") do
-    test = ANAGRAM.new()
-    expect(test.anagram("hi", "bye")).to(eq("These words have no letter matches and are antigrams."))
+    test = ANAGRAM.new("hi")
+    expect(test.anagram("bye")).to(eq("These words have no letter matches and are antigrams."))
   end
 
   it("5) returns 'These words are boring.' when either word entered contains at least one similar letter.") do
-    test = ANAGRAM.new()
-    expect(test.anagram("hey", "hello")).to(eq("These words are boring."))
+    test = ANAGRAM.new("hey")
+    expect(test.anagram("hello")).to(eq("These words are boring."))
   end
 
 
