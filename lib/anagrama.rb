36cb5f24af82a8c5a3ffe001426ffbccf0f33199
Noah Horwitz <nhhor@post.com>
2019-12-13 15:01:54 -0800

    correcting error for reg exp gsub.

--- lib/anagrama.rb
+++ lib/anagrama.rb
@@ -2,10 +2,14 @@ class ANAGRAM
     
   attr_reader(:primary_word)
   # attr_writer(:)
-  # attr_accessor(:)
+  attr_accessor(:anagram_word_array, :antigram_word_array, :boring_word_array, :error_array)
 
   def initialize(primary_word)
     @primary_word = primary_word
+    @anagram_word_array = []
+    @antigram_word_array = []
+    @boring_word_array = []
+    @error_array = []
   end
 
 
@@ -13,32 +17,73 @@ class ANAGRAM
     sorted_letters1 = primary_word.downcase.split("").sort
     sorted_letters2 = word2.downcase.split("").sort
     letters_compare = sorted_letters1 & sorted_letters2
+    word2_array = word2.gsub(/[^a-z~]/i, "").split("~")
 
-    # puts `clear`
     puts ""
+    puts "LOG word2_array: '#{word2_array}'"
     puts "LOG primary_word: '#{primary_word}' / LOG sorted_letters1: #{sorted_letters1}"
     puts "LOG word2: '#{word2}' / LOG sorted_letters2: #{sorted_letters2}"
-
     puts "LOG sorted_letters1 length: #{sorted_letters1.length} sorted_letters2 length: #{sorted_letters2.length}"
     puts "LOG letters_compare: #{letters_compare} letters_compare length: #{letters_compare.length}"
 
-    if (primary_word.match(/[aeiouy]/i) && word2.match(/[aeiouy]/i))
-      if (sorted_letters1 == sorted_letters2)
-        answer = "These words are anagrams."
-      elsif letters_compare.length == 0
-        answer = "These words have no letter matches and are antigrams."
-      elsif letters_compare.length >= 1
-        answer = "These words are boring."
+    word2_array.each do |word_to_compare|
+      sorted_letters1 = primary_word.downcase.split("").sort
+      sorted_letters2 = word_to_compare.downcase.split("").sort
+      letters_compare = sorted_letters1 & sorted_letters2
+
+      if (primary_word.match(/[aeiouy]/i) && word_to_compare.match(/[aeiouy]/i))
+        if (sorted_letters1 == sorted_letters2)
+          anagram_word_array.push(word_to_compare)
+          answer = ""
+        elsif letters_compare.length == 0
+          antigram_word_array.push(word_to_compare)
+          answer = ""
+        elsif letters_compare.length >= 1
+          boring_word_array.push(word_to_compare)
+          answer = ""
+        else
+          error_array.push("NOAH HAS A PROBLEM WITH HIS CODE...")
+        end
       else
-        answer = "Noah has a problem with his code..."
+        error_array.push("You need to input actual words!")
       end
-    else
-      answer = "You need to input actual words!"
     end
+
+    puts "LOG primary_word: '#{primary_word}'"
+    puts "LOG anagram_word_array: '#{anagram_word_array}'"
+    puts "LOG antigram_word_array: '#{antigram_word_array}'"
+    puts "LOG boring_word_array: '#{boring_word_array}'"
+    puts "LOG error_array: '#{error_array}'"
+
+    if error_array.length > 0
+      answer = error_array.join(" ")
+    else
+      answer = "From your primary word #{primary_word.upcase}: "
+    end
+
+    anagram_word_array.each { |anagram| answer += "and #{anagram.upcase} is an anagram, "}
+
+    antigram_word_array.each { |antigram| answer += "and #{antigram.upcase} is an antigram, "}
+
+    boring_word_array.each { |boring| answer += "and #{boring.upcase} is boring, "}
+
+    # if (primary_word.match(/[aeiouy]/i) && word2.match(/[aeiouy]/i))
+    #   if (sorted_letters1 == sorted_letters2)
+    #     answer = "These words are anagrams."
+    #   elsif letters_compare.length == 0
+    #     answer = "These words have no letter matches and are antigrams."
+    #   elsif letters_compare.length >= 1
+    #     answer = "These words are boring."
+    #   else
+    #     answer = "Noah has a problem with his code..."
+    #   end
+    # else
+    #   answer = "You need to input actual words!"
+    # end
     
 
     puts answer
-    answer
+    answer.gsub(/,\W$/, ".").gsub(/$(and )/i, "")
 
   end
 end
--- spec/anagrama_spec.rb
+++ spec/anagrama_spec.rb
@@ -3,34 +3,34 @@ require('anagrama.rb')
 
 describe('ANAGRAM#checker') do
 
-  it("1) returns 'These words are anagrams.' when anagram words are entered.") do
-    test = ANAGRAM.new("not")
-    expect(test.anagram("ton")).to(eq("These words are anagrams."))
-  end
+  # it("1) returns 'These words are anagrams.' when anagram words are entered.") do
+  #   test = ANAGRAM.new("not")
+  #   expect(test.anagram("ton")).to(eq("These words are anagrams."))
+  # end
 
-  it("2) returns 'These words are anagrams.' when anagram words are entered regardless of letter-case.") do
-    test = ANAGRAM.new("Tea")
-    expect(test.anagram("Eat")).to(eq("These words are anagrams."))
-  end
+  # it("2) returns 'These words are anagrams.' when anagram words are entered regardless of letter-case.") do
+  #   test = ANAGRAM.new("Tea")
+  #   expect(test.anagram("Eat")).to(eq("These words are anagrams."))
+  # end
 
-  it("3) returns 'You need to input actual words!' when either word entered does not contain at least one vowel.") do
-    test = ANAGRAM.new("Ruby")
-    expect(test.anagram("hbnR")).to(eq("You need to input actual words!"))
-  end
+  # it("3) returns 'You need to input actual words!' when either word entered does not contain at least one vowel.") do
+  #   test = ANAGRAM.new("Ruby")
+  #   expect(test.anagram("hbnR")).to(eq("You need to input actual words!"))
+  # end
 
-  it("4) returns 'These words have no letter matches and are antigrams.' when either word entered does not contain at least one similar letter.") do
-    test = ANAGRAM.new("hi")
-    expect(test.anagram("bye")).to(eq("These words have no letter matches and are antigrams."))
-  end
+  # it("4) returns 'These words have no letter matches and are antigrams.' when either word entered does not contain at least one similar letter.") do
+  #   test = ANAGRAM.new("hi")
+  #   expect(test.anagram("bye")).to(eq("These words have no letter matches and are antigrams."))
+  # end
 
-  it("5) returns 'These words are boring.' when either word entered contains at least one similar letter.") do
-    test = ANAGRAM.new("hey")
-    expect(test.anagram("hello")).to(eq("These words are boring."))
-  end
+  # it("5) returns 'These words are boring.' when either word entered contains at least one similar letter.") do
+  #   test = ANAGRAM.new("hey")
+  #   expect(test.anagram("hello")).to(eq("These words are boring."))
+  # end
 
   it("6) returns the anagram/antigram/status of each word entered as an argument compared to the primary word.") do
     test = ANAGRAM.new("parse")
-    expect(test.anagram("pe'ars~munch~spaz s")).to(eq("From your primary word of parse: pears is an anagram, munch is an antigram, and spazs is boring."))
+    expect(test.anagram("pe'ars~mu@nch~spaz s~spear")).to(eq("From your primary word of parse: pears is an anagram, spear is an anagram, munch is an antigram, spazs is boring."))
   end
 
 
