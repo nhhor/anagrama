a9ec8d9fba8da0e2c7d8800fb07eaacd76ee7af4
Noah Horwitz <nhhor@post.com>
2019-12-13 11:08:53 -0800

    complete code for 3rd spec. test passes.

--- lib/anagrama.rb
+++ lib/anagrama.rb
@@ -12,19 +12,26 @@ class ANAGRAM
     sorted_letters2 = word2.downcase.split("").sort
     letters_compare = sorted_letters1 & sorted_letters2
 
+    puts `clear`
+    puts ""
     puts "LOG word1: '#{word1}' / LOG sorted_letters1: #{sorted_letters1}"
     puts "LOG word2: '#{word2}' / LOG sorted_letters2: #{sorted_letters2}"
 
-    puts "LOG letters_compare: #{letters_compare}"
-    puts "LOG sorted_letters1 length: #{sorted_letters1.length}"
-    puts "LOG sorted_letters2 length: #{sorted_letters2.length}"
-    puts "LOG letters_compare length: #{letters_compare.length}"
+    puts "LOG sorted_letters1 length: #{sorted_letters1.length} sorted_letters2 length: #{sorted_letters2.length}"
+    puts "LOG letters_compare: #{letters_compare} letters_compare length: #{letters_compare.length}"
 
-    if (sorted_letters1 == sorted_letters2)
-      answer = "These words are anagrams."
+    if (word1.match(/[aeiouy]/) && word2.match(/[aeiouy]/))
+      if (sorted_letters1 == sorted_letters2)
+        answer = "These words are anagrams."
+      else
+        answer = "cant be anagram..."
+      end
     else
-      answer = "cant be anagram..."
+      answer = "You need to input actual words!"
     end
+    
+
+    puts answer
     answer
 
   end
