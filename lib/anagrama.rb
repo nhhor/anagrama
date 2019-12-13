0d6c0fa8fd07b38d36e167d354d645bbe0007fb7
Noah Horwitz <nhhor@post.com>
2019-12-13 10:15:18 -0800

    complete code for first spec. test passes.

--- lib/anagrama.rb
+++ lib/anagrama.rb
@@ -4,13 +4,28 @@ class ANAGRAM
   # attr_writer(:)
   # attr_accessor(:)
 
-  def initialize()
-
-  end
+  # def initialize()
+  # end
 
   def anagram(word1, word2)
-    "test"
+    sorted_letters1 = word1.split("").sort
+    sorted_letters2 = word2.split("").sort
+    letters_compare = sorted_letters1 & sorted_letters2
+
+    puts "LOG word1: '#{word1}' / LOG sorted_letters1: #{sorted_letters1}"
+    puts "LOG word2: '#{word2}' / LOG sorted_letters2: #{sorted_letters2}"
+
+    puts "LOG letters_compare: #{letters_compare}"
+    puts "LOG sorted_letters1 length: #{sorted_letters1.length}"
+    puts "LOG sorted_letters2 length: #{sorted_letters2.length}"
+    puts "LOG letters_compare length: #{letters_compare.length}"
+
+    if (sorted_letters1 == sorted_letters2)
+      answer = "These words are anagrams."
+    else
+      answer = "cant be anagram..."
+    end
+    answer
+
   end
-
-
 end
