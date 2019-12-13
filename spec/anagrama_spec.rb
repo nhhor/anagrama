71347686b05e7630250a432774cf78a17a276da2
Noah Horwitz <nhhor@post.com>
2019-12-13 10:20:19 -0800

    add test for 2nd spec, currently failing rspec.

--- spec/anagrama_spec.rb
+++ spec/anagrama_spec.rb
@@ -8,4 +8,9 @@ describe('ANAGRAM#checker') do
     expect(test.anagram("not", "ton")).to(eq("These words are anagrams."))
   end
 
+  it("returns 'These words are anagrams.' when anagram words are entered regardless of letter-case.") do
+    test = ANAGRAM.new()
+    expect(test.anagram("Tea", "Eat")).to(eq("These words are anagrams."))
+  end
+
 end
